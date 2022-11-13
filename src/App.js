e769c0cb13116e152da39d4c97814aa97bb5dc55
Yuyi Hua <yuyi@qivie.se>
2022-11-13 12:32:46 +0100

    changed background color

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="title-row">
-        <h1 className="title">Firebase Recipess</h1>
+        <h1 className="title">Qivie</h1>
       </div>
     </div>
   );
